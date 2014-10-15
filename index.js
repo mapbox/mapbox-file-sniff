017f29995d90114ab7a20f81f82ecf9e086d30ed
gretacb <carol@mapbox.com>
2014-10-15 14:10:30 -0400

    gunzip using pako, update tests, add more fixtures

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var zlib = require('zlib');
+var pako = require('pako');
 var fs = require('fs');
 var invalid = require('./lib/invalid');
 module.exports.sniff = sniff;
@@ -37,16 +37,20 @@ function sniff(filepath, callback) {
     if (((head.indexOf('<?xml') === 1) || (head.indexOf('<?xml') === 0)) && (head.indexOf('<gpx') !== -1)){
         return callback(null, 'gpx');    
     }
-    if (/^\s*\{/.test(head)){
+    if (head.indexOf('\"tilejson\":') !== -1){
         return callback(null, 'tilejson');
     }
-    zlib.gunzip(buffer, function(err, tar) {
-        if (err) return callback(invalid('Unknown filetype.'));
-        //check for tm2z
-        if (tar.slice(257, 262).toString() === 'ustar') return callback(null, 'tm2z');
-        //check for serial tiles
-        head = tar.slice(0,50).toString();
-        if (head.indexOf('JSONBREAKFASTTIME') === 0) return callback(null, 'serialtiles');
+        
+    var output;
+    try {
+        output = pako.inflate(buffer, {to: 'string'});
+    } catch (err) {
         return callback(invalid('Unknown filetype.'));
-    });
-}
+    }
+    //check for tm2z
+    if (output.slice(257, 262) === 'ustar') return callback(null, 'tm2z');
+    //check for serial tiles
+    head = output.slice(0,50);
+    if (head.indexOf('JSONBREAKFASTTIME') === 0) return callback(null, 'serialtiles');
+    else return callback(invalid('Unknown filetype.'));
+};
--- test/index.test.js
+++ test/index.test.js
@@ -90,8 +90,22 @@ tape('[TIF] Sniffing file: should return tif filetype', function(assert) {
         assert.end();
     });
 });
-tape('[tilejson good] Sniffing file: should return tilejson filetype', function(assert) {
-	var file = path.resolve('./test/data/good.tilejson');
+tape('[mbtiles] Sniffing file: should return mbtiles filetype', function(assert) {
+    var file = path.resolve('./test/data/valid.mbtiles');
+    var expectedFiletype = 'mbtiles';
+    filesniffer.sniff(file, function(err, filetype) {
+        if (err) return assert.end(err);
+        assert.ok(err === null);
+        try {
+            assert.equal(filetype, expectedFiletype);
+        } catch (err) {
+            console.log(err);
+        }
+        assert.end();
+    });
+});
+tape('[tilejson Valid] Sniffing file: should return tilejson filetype', function(assert) {
+	var file = path.resolve('./test/data/valid.tilejson');
     var expectedFiletype = 'tilejson';
     filesniffer.sniff(file, function(err, filetype) {
     	if (err) return assert.end(err);
@@ -104,36 +118,58 @@ tape('[tilejson good] Sniffing file: should return tilejson filetype', function(
         assert.end();
     });
 });
-// tape('[tilejson Bad] Sniffing file: should return error', function(assert) {
-// 	var file = path.resolve('./test/data/bad.tilejson');
-//     filesniffer.sniff(file, function(err) {
-//         console.log(err);
-//         assert.ok(err instanceof Error);
-//         assert.error(err, 'Unknown filetype.');
-//         assert.equal('EINVALID', err.code);
-//         assert.end();
-//     });
-// });
-// tape('[mbtiles Bad] Sniffing file: should return error', function(assert) {
-// 	var file = path.resolve('./test/data/bad.mbtiles');
-//     filesniffer.sniff(file, function(err) {
-//         assert.ok(err instanceof Error);
-//         assert.error(err, 'Unknown filetype.');
-//         assert.equal('EINVALID', err.code);
-//         assert.end();
-//     });
-// });
-tape('[mbtiles good] Sniffing file: should return mbtiles filetype', function(assert) {
-	var file = path.resolve('./test/data/good.mbtiles');
-    var expectedFiletype = 'mbtiles';
+tape('[tilejson Invalid] Sniffing file: should return error', function(assert) {
+	var file = path.resolve('./test/data/invalid.tilejson');
+    filesniffer.sniff(file, function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Unknown filetype.');
+        assert.equal('EINVALID', err.code);
+        assert.end();
+    });
+});
+tape('[serialtiles] Sniffing file: should return serialtiles filetype', function(assert) {
+    var file = path.resolve('./test/data/valid-serialtiles.gz');
+    var expectedFiletype = 'serialtiles';
     filesniffer.sniff(file, function(err, filetype) {
-    	if (err) return assert.end(err);
+        if (err) return assert.end(err);
         assert.ok(err === null);
         try {
-	        assert.equal(filetype, expectedFiletype);
+            assert.equal(filetype, expectedFiletype);
         } catch (err) {
-        	console.log(err);
+            console.log(err);
         }
+        assert.end();
+    });
+});
+tape('[tm2z] Sniffing file: should return tm2z filetype', function(assert) {
+    var file = path.resolve('./test/data/valid.tm2z');
+    var expectedFiletype = 'tm2z';
+    filesniffer.sniff(file, function(err, filetype) {
+        if (err) return assert.end(err);
+        assert.ok(err === null);
+        try {
+            assert.equal(filetype, expectedFiletype);
+        } catch (err) {
+            console.log(err);
+        }
+        assert.end();
+    });
+});
+tape('[tm2z Invalid malformed] Sniffing file: should return error', function(assert) {
+    var file = path.resolve('./test/data/invalid-malformed.tm2z');
+    filesniffer.sniff(file, function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Unknown filetype.');
+        assert.equal('EINVALID', err.code);
+        assert.end();
+    });
+});
+tape('[tm2z Invalid empty] Sniffing file: should return error', function(assert) {
+    var file = path.resolve('./test/data/invalid-empty.tm2z');
+    filesniffer.sniff(file, function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Unknown filetype.');
+        assert.equal('EINVALID', err.code);
         assert.end();
     });
 });
