55f535f221f0d6da914ce6e4d595f8e16ae45203
Ryan Clark <ryan.clark.j@gmail.com>
2014-10-28 14:55:31 -0700

    don't read the whole file in command-line tools

--- bin/mapbox-file-protocol.js
+++ bin/mapbox-file-protocol.js
@@ -5,11 +5,13 @@ var fs = require('fs');
 var path = require('path');
 var filepath = path.resolve(process.argv[2]);
 
-fs.exists(filepath, function(exists) {
-    if (!exists) return console.error(new Error('File does not exist'));
-    fs.readFile(filepath, function(err, buf) {
-        if (err) return console.error(err);
-        filesniffer.protocol(buf, function(err, protocol) {
+fs.open(filepath, 'r', function(err, fd) {
+    if (err) return console.error(err);
+    var buf = new Buffer(512);
+    fs.read(fd, buf, 0, 512, 0, function(err, bytes, buffer) {
+        if (bytes.length < 300)
+          return console.error(new Error('File too small'));
+        filesniffer.protocol(buffer, function(err, protocol) {
             if (err) return console.error(err);
             console.log(protocol);
         });
