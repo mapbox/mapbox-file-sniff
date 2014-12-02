ac6b282ca5841dc12c7c5504ceda45c86e4a24dd
Ryan Clark <ryan.clark.j@gmail.com>
2014-12-02 11:04:04 -0700

    exit codes + quaffing for executables

--- bin/mapbox-file-protocol.js
+++ bin/mapbox-file-protocol.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 
 var filesniffer = require('..');
-var fs = require('fs');
 var path = require('path');
 var filepath = path.resolve(process.argv[2]);
 
-fs.open(filepath, 'r', function(err, fd) {
-    if (err) return console.error(err);
-    var buf = new Buffer(512);
-    fs.read(fd, buf, 0, 512, 0, function(err, bytes, buffer) {
-        if (bytes.length < 300)
-          return console.error(new Error('File too small'));
-        filesniffer.waft(buffer, function(err, protocol) {
-            if (err) return console.error(err);
-            console.log(protocol);
-        });
-    });
+filesniffer.quaff(filepath, true, function(err, protocol) {
+    if (err) {
+        console.error(err.message);
+        process.exit(err.code === 'EINVALID' ? 3 : 1);
+    }
+    console.log(protocol);
 });
